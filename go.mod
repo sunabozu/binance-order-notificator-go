4a3db1f44c19990749e9e374fe5307c2e1284029
troorl <troorl@troorls-MacBook-Air.local>
2020-07-10 20:51:14 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sunabozu/binance-order-notificator-go
+
+go 1.14
+
+require (
+	github.com/adshao/go-binance v0.0.0-20200615152609-3c91c9175d97
+	github.com/sunabozu/binance-price-change-go v0.0.0-20200710173312-377ba47313f1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/sunabozu/binance-price-change-go/utils"
+
+	"github.com/adshao/go-binance"
+)
+
+func main() {
+	parentPath, err := utils.GetParentPath()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	keys, err := utils.LoadKeys(parentPath + "/env.json")
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	client := binance.NewClient(keys.BinanceKey, keys.BinanceSecret)
+	listenKey, err := client.NewStartUserStreamService().Do(context.Background())
+
+	log.Printf("%+v", listenKey)
+
+	wsHandler := func(message []byte) {
+		var userData map[string]interface{}
+
+		if err := json.Unmarshal(message, &userData); err != nil {
+			panic(err)
+		}
+
+		if userData["e"] != "executionReport" || userData["X"] != "FILLED" {
+			return
+		}
+
+		price, err := strconv.ParseFloat(userData["p"].(string), 64)
+		quantity, err := strconv.ParseFloat(userData["q"].(string), 64)
+
+		if err != nil {
+			return
+		}
+
+		usdValue := quantity * price
+		symbol := userData["s"].(string)
+		side := userData["S"].(string)
+		currency := "฿"
+		emoji := "⬇️⬇️⬇️"
+		priceStr := fmt.Sprintf("%.2f", price)
+		usdValueStr := fmt.Sprintf("%.2f", usdValue)
+
+		if symbol[len(symbol)-4:] == "USDT" {
+			symbol = symbol[0 : len(symbol)-4]
+			currency = "$"
+		} else if symbol[len(symbol)-3:] == "BTC" {
+			symbol = symbol[0 : len(symbol)-3]
+			priceStr = fmt.Sprintf("%.8f", price)
+			usdValueStr = fmt.Sprintf("%.8f", usdValue)
+		}
+
+		content := fmt.Sprintf("%.6f %s (%s%s)", quantity, symbol, currency, usdValueStr)
+
+		if side == "BUY" {
+			content += " was BOUGHT"
+		} else {
+			content += " was SOLD"
+			emoji = "✅✅✅"
+		}
+
+		content += fmt.Sprintf(" at the price of %s %s", priceStr, emoji)
+
+		log.Println(content)
+
+		go utils.SendPushNotification(keys, content)
+	}
+
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+
+	doneC, _, err := binance.WsUserDataServe(listenKey, wsHandler, errHandler)
+	<-doneC
+}
